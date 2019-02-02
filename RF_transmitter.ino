a20296663e918711e0b3f337bbe9cfb88087ba3e
pajeet063 <39622164+mhspes@users.noreply.github.com>
2019-02-02 16:29:30 +0200

    Add files via upload

--- RF_transmitter.ino
+++ RF_transmitter.ino
@@ -1,31 +1,27 @@
-/*
-  433 MHz RF Module Transmitter Demonstration 1
-  RF-Xmit-Demo-1.ino
-  Demonstrates 433 MHz RF Transmitter Module
-  Use with Receiver Demonstration 1
 
-  DroneBot Workshop 2018
-  https://dronebotworkshop.com
-*/
+#include <RH_ASK.h>   // Kirjasto ASK-modulaatioon
+#include <SPI.h>      // Kirjasto synkroniseen tiedonsiirtoon
 
-// Include RadioHead Amplitude Shift Keying Library
-#include <RH_ASK.h>
-// Include dependant SPI Library 
-#include <SPI.h> 
+#define sensorPin 0  // Lämpötilasensorin analog-pinni
 
-// Create Amplitude Shift Keying Object
-RH_ASK rf_driver;
+RH_ASK rf_sender; 
 
-void setup()
-{
-    // Initialize ASK Object
-    rf_driver.init();
+// Asetukset
+void setup(){
+    rf_sender.init(); // rf-olion alustus
 }
 
-void loop()
-{
-    const char *msg = "Welcome to the Workshop!";
-    rf_driver.send((uint8_t *)msg, strlen(msg));
-    rf_driver.waitPacketSent();
-    delay(1000);
+// Loop:n sisältöä suoritetaan non-stoppina
+void loop(){
+    float analogIn = analogRead(sensorPin);         // Sensorista luettu arvo [0..1023]
+    float temp = (1023.0*10000)/analogIn-10000;       // Skaalattu arvo Kelvineiksi
+    float temp1 = (3435.0/(log(temp/10000)+(3435.0/(273.15+25)))) - 273.15;
+    //Lämpötilan lasku vastuksen resistanssista
+
+    String lampoString = String(temp1);
+    static char *msg = (lampoString).c_str();       // Parsitaan C++:n char-jonoksi
+    
+    rf_sender.send((uint8_t *)msg, strlen(msg));    // Lähetys
+    rf_sender.waitPacketSent();                   
+    delay(500);                                     // Odotetaan 0.5s
 }
