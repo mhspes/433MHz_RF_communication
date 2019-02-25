fa38c629f6ffc6001b70503f748b16508e40d9c2
Mikko Pesonen <39622164+mhspes@users.noreply.github.com>
2019-02-25 20:02:17 +0200

    Update RF_transmitter.ino

--- RF_transmitter.ino
+++ RF_transmitter.ino
@@ -11,17 +11,16 @@ void setup(){
     rf_sender.init(); // rf-olion alustus
 }
 
-// Loop:n sisältöä suoritetaan non-stoppina
 void loop(){
-    float analogIn = analogRead(sensorPin);         // Sensorista luettu arvo [0..1023]
-    float temp = (1023.0*10000)/analogIn-10000;       // Skaalattu arvo Kelvineiksi
+    float analogIn = analogRead(sensorPin);         // Sensorista luettu analoginen arvo [0..1023]
+    float temp = (1023.0*10000)/analogIn-10000;       // Skaalaus kelvineiksi
     float temp1 = (3435.0/(log(temp/10000)+(3435.0/(273.15+25)))) - 273.15;
-    //Lämpötilan lasku vastuksen resistanssista
+    //Lämpötilan laskenta vastuksen resistanssista
 
     String lampoString = String(temp1);
-    static char *msg = (lampoString).c_str();       // Parsitaan C++:n char-jonoksi
+    static char *msg = (lampoString).c_str();       // Parsitaan muotoon char*
     
     rf_sender.send((uint8_t *)msg, strlen(msg));    // Lähetys
     rf_sender.waitPacketSent();                   
-    delay(500);                                     // Odotetaan 0.5s
+    delay(500);
 }
